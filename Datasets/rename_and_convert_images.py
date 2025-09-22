86da1690520c2902309203fdeadc7ad7f28bb712
Phuoc Hieu <hieuphamminh976@gmail.com>
2025-09-22 20:13:54 +0700

    Update

--- Datasets/rename_and_convert_images.py
+++ Datasets/rename_and_convert_images.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 import re
-Image.MAX_IMAGE_PIXELS = None
+Image.MAX_IMAGE_PIXELS = None  
 
 def rename_and_convert_images(folder_path, prefix="image", target_ext=".jpg"):
     files = os.listdir(folder_path)
@@ -26,13 +26,15 @@ def rename_and_convert_images(folder_path, prefix="image", target_ext=".jpg"):
         src = os.path.join(folder_path, filename)
         img = Image.open(src).convert("RGB")
 
+        img = img.resize((256, 256), Image.Resampling.LANCZOS)
+
         counter += 1
         new_name = f"{prefix}_{counter}{target_ext}"
         dst = os.path.join(folder_path, new_name)
         img.save(dst)
         os.remove(src)  
 
-    print(f"Updated {folder_path}: {counter - max_idx} new images converted.")
+    print(f"Updated {folder_path}: {counter - max_idx} new images converted & resized to 256x256.")
 
 rename_and_convert_images("front", prefix="front", target_ext=".jpg")
 rename_and_convert_images("front_left", prefix="front_left", target_ext=".jpg")
@@ -41,3 +43,5 @@ rename_and_convert_images("rear", prefix="rear", target_ext=".jpg")
 rename_and_convert_images("rear_left", prefix="rear_left", target_ext=".jpg")
 rename_and_convert_images("rear_right", prefix="rear_right", target_ext=".jpg")
 rename_and_convert_images("high_angle_view", prefix="high_angle_view", target_ext=".jpg")
+rename_and_convert_images("images", prefix="images", target_ext=".jpg")
+rename_and_convert_images("Test", prefix="test", target_ext=".jpg")
